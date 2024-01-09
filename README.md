bd3c94fad54a7e06105a36352589b446bf0068fd
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-01-09 16:02:15 +0100

    Final

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # test-swin
 
-## Requirements
+## Usage
 
 1. Ninja (optional but recommended)
 2. C++ compiler and CUDA compiler compatible among them (see for a matrix of compatible versions https://gist.github.com/ax3l/9489132#nvcc)
 3. Python headers available:
    - you can install `python3-dev` packages or similar
    - you can set the CPATH environment variable to point to the python headers directory
-   - you can use `pyenv exec pip install ...` when installing this packagehttps://gist.github.com/ax3l/9489132#nvcc
+   - you can use `pyenv exec pip install ...` when installing this packages
+4. `pdm` (consider installing it via `pipx`)
+5. `pdm install --no-self --no-isolation`
+6. Download the weights from the SwinDocSegmenter repo
+7. Fix the paths to the weights in `test.py`
+8. Run `pdm run test.py`
+9. Check the `test.png` image
 
-N.B. Tensorflow (DE-GAN) may requires NCCL
+N.B. Tensorflow may requires NCCL (not sure)
--- test.py
+++ test.py
@@ -9,6 +9,32 @@ from detectron2.projects.deeplab import add_deeplab_config
 
 from maskdino.config import add_maskformer2_config
 
+WEIGHTS_PATH = "../swindocsegmenter/model_final_prima_swindocseg.pth"
+
+# Class names in the proper order
+CLASSES = np.asarray(
+    [
+        "background",
+        "textregion",
+        "imageregion",
+        "tableregion",
+        "mathsregion",
+        "separatorregion",
+        "otherregion",
+    ]
+)
+
+# Color for each class
+COLORS = [
+    (255, 0, 0),
+    (0, 255, 0),
+    (0, 0, 255),
+    (255, 255, 0),
+    (0, 255, 255),
+    (255, 0, 255),
+    (255, 255, 255),
+]
+
 
 def setup(args):
     """
@@ -34,52 +60,6 @@ def setup(args):
     return cfg
 
 
-cfg = setup({"config_file": "./config_prima.yaml", "opts": []})
-model = build_model(cfg)  # returns a torch.nn.module
-
-DetectionCheckpointer(model).load(
-    "../swindocsegmenter/model_final_prima_swindocseg.pth"
-)  # load a file, usually from cfg.model.weights
-
-img = cv2.imread("../LaudareAugmentation/image_tests/laudare/Cortona1.png")
-# resize for faster inference and RAM saving
-img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
-# img = cv2.resize(img, (1024, 1024))
-# bgr to rgb
-img_ = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-# hwc to chw format
-img_ = torch.tensor(img_).permute(2, 0, 1)
-# float32 format
-img_ = img_.float()
-
-classes = np.asarray(
-    [
-        "background",
-        "textregion",
-        "imageregion",
-        "tableregion",
-        "mathsregion",
-        "separatorregion",
-        "otherregion",
-    ]
-)
-
-model.eval()
-with torch.no_grad():
-    outputs = model([{"image": img_, "height": img_.shape[1], "width": img_.shape[2]}])
-
-
-COLORS = [
-    (255, 0, 0),
-    (0, 255, 0),
-    (0, 0, 255),
-    (255, 255, 0),
-    (0, 255, 255),
-    (255, 0, 255),
-    (255, 255, 255),
-]
-
-
 def draw_box(img, box, color, label):
     # box: [x0,y0,x1,y1]
     box = [round(x) for x in box]
@@ -87,7 +67,7 @@ def draw_box(img, box, color, label):
 
     # draw a bounding box rectangle and label on the image
     cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), color, 2)
-    text = "{}: {:.4f}".format(classes[label], 1.0)
+    text = "{}: {:.4f}".format(CLASSES[label], 1.0)
     cv2.putText(
         img,
         text,
@@ -101,26 +81,35 @@ def draw_box(img, box, color, label):
     return img
 
 
+# Load model and weights
+cfg = setup({"config_file": "./config_prima.yaml", "opts": []})
+model = build_model(cfg)  # returns a torch.nn.module
+DetectionCheckpointer(model).load(WEIGHTS_PATH)
+
+img = cv2.imread("./example.jpg")
+# resize for faster inference and RAM saving
+img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
+# img = cv2.resize(img, (1024, 1024))
+# bgr to rgb
+img_ = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# hwc to chw format
+img_ = torch.tensor(img_).permute(2, 0, 1)
+# float32 format
+img_ = img_.float()
+
+# inference
+model.eval()
+with torch.no_grad():
+    outputs = model([{"image": img_, "height": img_.shape[1], "width": img_.shape[2]}])
+
+# Draw boxes on image
 for output in outputs:
     pred_classes = output["instances"].pred_classes.cpu().numpy()
     boxes = output["instances"].pred_boxes.tensor.cpu().numpy()
-    # normalize boxes so that min-max is 0-1
-    min_x = boxes[:, 0].min()
-    min_y = boxes[:, 1].min()
-    max_x = boxes[:, 2].max()
-    max_y = boxes[:, 3].max()
-    boxes[:, 0] = (boxes[:, 0] - min_x) / (max_x - min_x)
-    boxes[:, 1] = (boxes[:, 1] - min_y) / (max_y - min_y)
-    boxes[:, 2] = (boxes[:, 2] - min_x) / (max_x - min_x)
-    boxes[:, 3] = (boxes[:, 3] - min_y) / (max_y - min_y)
-    # denormalize boxes to original image size
-    boxes[:, 0] = boxes[:, 0] * img.shape[1]
-    boxes[:, 1] = boxes[:, 1] * img.shape[0]
-    boxes[:, 2] = boxes[:, 2] * img.shape[1]
-    boxes[:, 3] = boxes[:, 3] * img.shape[0]
 
     # draw boxes on image
     for i, box in enumerate(boxes):
         img = draw_box(img, box, pred_classes[i], pred_classes[i])
 
-    cv2.imwrite("test.png", img)
+# save image
+cv2.imwrite("example_segmented.png", img)
