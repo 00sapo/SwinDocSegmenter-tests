b613883133e001966a26465372a20c54e2d02c77
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-01-09 16:15:23 +0100

    Added test for cortonese

--- test.py
+++ test.py
@@ -86,30 +86,46 @@ cfg = setup({"config_file": "./config_prima.yaml", "opts": []})
 model = build_model(cfg)  # returns a torch.nn.module
 DetectionCheckpointer(model).load(WEIGHTS_PATH)
 
-img = cv2.imread("./example.jpg")
-# resize for faster inference and RAM saving
-img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
-# img = cv2.resize(img, (1024, 1024))
-# bgr to rgb
-img_ = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-# hwc to chw format
-img_ = torch.tensor(img_).permute(2, 0, 1)
-# float32 format
-img_ = img_.float()
+img1 = cv2.imread("./example0.jpg")
+img2 = cv2.imread("./example1.png")
+
+
+def preprocess_image(img: np.ndarray):
+    # resize for faster inference and RAM saving
+    img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
+    # img = cv2.resize(img, (1024, 1024))
+    # bgr to rgb
+    img_ = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # hwc to chw format
+    img_ = torch.tensor(img_).permute(2, 0, 1)
+    # float32 format
+    img_ = img_.float().cuda()
+    return img_
+
 
 # inference
 model.eval()
 with torch.no_grad():
-    outputs = model([{"image": img_, "height": img_.shape[1], "width": img_.shape[2]}])
+    for j, img in enumerate([img1, img2]):
+        img_ = preprocess_image(img)
+        outputs = model(
+            [{"image": img_, "height": img_.shape[1], "width": img_.shape[2]}]
+        )
 
-# Draw boxes on image
-for output in outputs:
-    pred_classes = output["instances"].pred_classes.cpu().numpy()
-    boxes = output["instances"].pred_boxes.tensor.cpu().numpy()
+        # Draw boxes on image
+        for output in outputs:
+            pred_classes = output["instances"].pred_classes.cpu().numpy()
+            boxes = output["instances"].pred_boxes.tensor.cpu().numpy()
+            scores = output["instances"].scores.cpu().numpy()
 
-    # draw boxes on image
-    for i, box in enumerate(boxes):
-        img = draw_box(img, box, pred_classes[i], pred_classes[i])
+            # filter out low confidence boxes
+            boxes = boxes[scores > 0.5]
+            pred_classes = pred_classes[scores > 0.5]
 
-# save image
-cv2.imwrite("example_segmented.png", img)
+            # draw boxes on image
+            for i, box in enumerate(boxes):
+                box *= 2
+                img = draw_box(img, box, pred_classes[i], pred_classes[i])
+
+        # save image
+        cv2.imwrite(f"example_segmented{j}.png", img)
